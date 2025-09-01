f04c55f163cf6dc27a0df2c0808906361a2cdca7
anshika jain <Y226101005@dhsgsu.edu.in>
2025-09-02 00:45:36 +0530

    Create errors.js

---
+++ src/src/routes/src/store/src/middleware/errors.js
@@ -0,0 +1,14 @@
+export const notFound = (req, res, next) => {
+  res.status(404);
+  next(new Error(`Not Found - ${req.originalUrl}`));
+};
+
+export const errorHandler = (err, req, res, next) => {
+  // Use set status if not set
+  const status = res.statusCode && res.statusCode !== 200 ? res.statusCode : 500;
+  res.status(status).json({
+    message: err.message,
+    // hide stack in production
+    stack: process.env.NODE_ENV === 'production' ? undefined : err.stack
+  });
+};
